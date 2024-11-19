1f97fd80cca8ed0e9b36be36d3db31d0b5d2e654
Khasan <khasan.irgashev.97@gmail.com>
2024-11-19 08:56:59 +0500

    Изменил работу после завершения заявки, улушил текст сообщения. Чтоб одну заявку несколько раз нельзя было завершить

--- handlers/callback_handler.py
+++ handlers/callback_handler.py
@@ -56,12 +56,21 @@ async def callback_failed(call: CallbackQuery):
 async def callback_finished(call: CallbackQuery):
     text = call.message.text.split('\n')
     text = [x.split(':') for x in text]
-    text[-1][1] = "Выполнено‼️‼️‼️"
-    id_user = [x for x in text if 'ID Создателя' in x][0][1].strip()
-    request_id = text[0][1]
+    # print(text)
+    user_id = text[6][1].strip()
+    # print(user_id)
+    # print(call.message.chat.id)
+    text[-1][1] = "Выполнено"
+    #исправить работу после завершения
+    request_id = text[1][1].strip()
+    # print(request_id)
     status_key = 'request_status'
     status_value = text[-1][1]
+    # print(status_value)
     change_value_request(request_id, status_key, status_value)
     result = [':'.join(x) for x in text]
     result = '\n'.join(result)
-    await call.bot.send_message(chat_id=id_user, text=result)
+    # print(result)
+    await call.bot.send_message(chat_id=user_id, text=result+'‼️‼️‼️')
+    await call.message.edit_text(result+'‼️‼️‼️')
+    await call.bot.send_message(chat_id=CHANNEL_ID_ADMIN, text=result+'‼️‼️‼️')
